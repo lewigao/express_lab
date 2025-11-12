6845e1135412da976bb1efa1a6feb5d2e3e4a304
lewigao <lewigao@gmail.com>
2025-11-12 13:48:27 -0500

    added: route logic for id

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,16 @@ router.get("/new", (req, res) => {
     res.send("New User Form");
 });
 
+// router.get('/:id', (req, res)=> {
+//     res.send(`Getting User data: ${req.params.id}`)
+// });
+
+router.route("/:id").get((req, res) => {
+    res.send(`Getting User data: ${req.params.id}`);
+}).delete((req, res) => {
+    res.send(`Deleting user with ID: ${req.params.id}`);
+}).put((req, res) => {
+    res.send(`Updating user with ID: ${req.params.id}`);
+});
+
 module.exports = router;
